06c39b169d0e63efc15bcb4eef56057dbedf826d
Ksingh210 <48933892+Ksingh210@users.noreply.github.com>
2019-10-20 18:09:39 -0700

    debug change

--- myportforliosite/settings.py
+++ myportforliosite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5-3c%7ts8e=)zc_zm0rcp*i6$xb3yevpu-w7)y0&g4m42m^(k888'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
